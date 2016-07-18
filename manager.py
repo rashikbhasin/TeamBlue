c6a5b33a4e67ac1ddca3f561e400daecfae97c56
raghava95 <b.raghavendrarao@hotmail.com>
2016-07-18 16:49:29 +0530

    Changes Made

--- manager.py
+++ manager.py
@@ -7,24 +7,41 @@ class Manager(object):
          pass
 
     def add(self,event):
-        jsonObj = JsonHandler()
-        jsonObj.add_events(event)
+        data = None
+        with open(self._filename, 'r') as data_file:
+            data = json.load(data_file)
+        d = {event.get_event_id(): event.toJson()}
+        data.update(d)
+        with open(self._filename, 'w+') as changed:
+            changed.write(json.dumps(data, indent=4))
 
 
     def delete(self,event_id):
-        jsonObj=JsonHandler()
-        jsonObj.delete_event(event_id)
-
+        data = None
+        with open(self._filename, 'r') as data_file:
+            data = json.load(data_file)
+        del data[event_id]
+        with open(self._filename, 'w+') as changed:
+            changed.write(json.dumps(data, indent=4))
 
     def update(self,event):
-        jsonObj = JsonHandler()
-        jsonObj.update_event(event)
-
+        data = None
+        with open(self._filename, 'r') as data_file:
+            data = json.load(data_file)
+        d = event.toJson()
+        data[event.get_event_id()] = d
+        with open(self._filename, 'w+') as changed:
+            changed.write(json.dumps(data, indent=4))
     def read(self,event_id):
-        jsonObj = JsonHandler()
-        return jsonObj.read_event(event_id)
+        data = None
+        with open(self._filename, 'r') as data_file:
+            data = json.load(data_file)
+        event = Event(event_id, data[event_id]['name'], data[event_id]['info'], data[event_id]['date'],
+                      data[event_id]['venue'], data[event_id]['city'])
+        return event
 
-    def upcoming(self):
+
+def upcoming(self):
         jsonObj = JsonHandler()
         return jsonObj.upcoming_events()
 
@@ -43,3 +60,5 @@ class Manager(object):
     def list_events(self, date1, date2):
         jsonObj = JsonHandler()
         return jsonObj.list_events(date1, date2)
+
+   
