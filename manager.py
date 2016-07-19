78fa8a98e189d9f32a5d90fd3c152d9dcdf1fab4
raghava95 <b.raghavendrarao@hotmail.com>
2016-07-19 12:25:37 +0530

    Changes Made At Manager class

--- manager.py
+++ manager.py
@@ -1,3 +1,5 @@
+import json
+import datetime
 from jsonHandler import *
 from events import *
 
@@ -7,58 +9,87 @@ class Manager(object):
          pass
 
     def add(self,event):
-        data = None
-        with open(self._filename, 'r') as data_file:
-            data = json.load(data_file)
-        d = {event.get_event_id(): event.toJson()}
-        data.update(d)
-        with open(self._filename, 'w+') as changed:
-            changed.write(json.dumps(data, indent=4))
+        jsonObj = JsonHandler()
+        jsonObj.add_events(event)
 
 
     def delete(self,event_id):
-        data = None
-        with open(self._filename, 'r') as data_file:
-            data = json.load(data_file)
-        del data[event_id]
-        with open(self._filename, 'w+') as changed:
-            changed.write(json.dumps(data, indent=4))
+        jsonObj=JsonHandler()
+        jsonObj.delete_event(event_id)
+
 
     def update(self,event):
-        data = None
-        with open(self._filename, 'r') as data_file:
-            data = json.load(data_file)
-        d = event.toJson()
-        data[event.get_event_id()] = d
-        with open(self._filename, 'w+') as changed:
-            changed.write(json.dumps(data, indent=4))
+        jsonObj = JsonHandler()
+        jsonObj.update_event(event)
+
     def read(self,event_id):
+        jsonObj = JsonHandler()
+        return jsonObj.read_event(event_id)
+
+    def upcoming(self):
         data = None
-        with open(self._filename, 'r') as data_file:
+        filename="json_data.json"
+        with open(filename, 'r') as data_file:
             data = json.load(data_file)
-        event = Event(event_id, data[event_id]['name'], data[event_id]['info'], data[event_id]['date'],
-                      data[event_id]['venue'], data[event_id]['city'])
-        return event
-
-
-def upcoming(self):
-        jsonObj = JsonHandler()
-        return jsonObj.upcoming_events()
-
+        today = str(datetime.date.today())
+        lst = []
+        for i in data:
+            if data[i]['date'] > today:
+                event = Event(i, data[i]['name'], data[i]['info'], data[i]['date'], data[i]['venue'], data[i]['city'])
+                lst.append(event)
+        return lst
+    
+    
+    
     def completed(self):
-        jsonObj = JsonHandler()
-        return jsonObj.completed_events()
-
+        data = None
+        filename="json_data.json"
+        with open(filename, 'r') as data_file:
+            data = json.load(data_file)
+        today = str(datetime.date.today())
+        lst = []
+        for i in data:
+            if data[i]['date'] < today:
+                event = Event(i, data[i]['name'], data[i]['info'], data[i]['date'], data[i]['venue'], data[i]['city'])
+                lst.append(event)
+        return lst
+    
+    
     def search_city(self, city):
-        jsonObj = JsonHandler()
-        return jsonObj.search_events_city(city)
-
+        data = None
+        filename="json_data.json"
+        with open(filename, 'r') as data_file:
+            data = json.load(data_file)
+        lst = []
+        for i in data:
+            if data[i]['city'] == city:
+                event = Event(i, data[i]['name'], data[i]['info'], data[i]['date'], data[i]['venue'], data[i]['city'])
+                lst.append(event)
+        return lst
+    
+    
     def search_date(self, date):
-        jsonObj = JsonHandler()
-        return jsonObj.search_events_date(date)
+        data = None
+        filename="json_data.json"    
+        with open(filename, 'r') as data_file:
+            data = json.load(data_file)
+        lst = []
+        for i in data:
+            if data[i]['date'] == date:
+                event = Event(i, data[i]['name'], data[i]['info'], data[i]['date'], data[i]['venue'], data[i]['city'])
+                lst.append(event)
+        return lst
+
 
     def list_events(self, date1, date2):
-        jsonObj = JsonHandler()
-        return jsonObj.list_events(date1, date2)
+        data = None
+        filename="json_data.json"        
+        with open(filename, 'r') as data_file:
+            data = json.load(data_file)
+        lst = []
+        for i in data:
+            if data[i]['date'] > date1 and data[i]['date'] < date2:
+                event = Event(i, data[i]['name'], data[i]['info'], data[i]['date'], data[i]['venue'], data[i]['city'])
+                lst.append(event)
+        return lst
 
-   
